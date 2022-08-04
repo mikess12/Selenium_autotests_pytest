c7979acbf632688633bbb2f88cb4bec5eaf6a729
mikess12 <106674394+mikess12@users.noreply.github.com>
2022-08-04 11:50:14 +0300

    Add_login_autotest_pytest

---
+++ test_login_find.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver import Keys
+import time
+
+def test_setup():
+    global browser
+    browser = webdriver.Chrome("C:\\Users\\trcmi\\Desktop\\Testing\\Курс Медани\\sts_tests\\chromedriver.exe")
+
+def test_login():
+    browser.get('https://sandbox.synctoskill.com/')
+    browser.maximize_window()
+    browser.find_elements("xpath", '//a[@class="nav-link text-dark"]')[0].click() #кликаем на кнопку логина
+    browser.find_element("name", 'Email').send_keys('sfanat@yandex.ru')   #вводим почту
+    browser.find_element("name", 'Password').send_keys('Merinda4+')
+    #browser.find_element("xpath", '//input[@class="button"]').click()
+    browser.find_element("xpath", '//input[@value="Sign In"]').click()
+    browser.find_element("xpath", '//a[@href="/Account/Profile"]').get_attribute('text').find("Mikhail Filimonov")
+def test_search():
+    browser.find_element("xpath", '//input[@class ="searchBar"]').send_keys('avocado')  #найти строку поиска и вставить туда слово авокадо
+    browser.find_element("xpath", '//input[@class ="searchBar"]').send_keys(Keys.ENTER) #нажать клавишу Enter
+    # browser.get_screenshot_as_file('3.png') 
+    time.sleep(5)
+    browser.close()
+
+
+
+
+
+
+
+
+
+
