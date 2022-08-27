cab797482ed544e44f98bcd40fcabace544e36d7
Mikhail Filimonov <106674394+mikess12@users.noreply.github.com>
2022-08-27 13:10:59 +0300

    Update test_api_login_autotest.py

--- test_api_login_autotest.py
+++ test_api_login_autotest.py
@@ -1,4 +1,3 @@
-from urllib import response
 import requests
 import json
 
@@ -6,21 +5,24 @@ with open('creds.json', 'r') as f:
     config = json.load(f)
 
 
-#def test_api_login():
+def test_api_login():
 
-headers = {
-"User-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
-}
+    headers = {
+    "User-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
+    }
 
-data = {
-"email": config["email"],
-"password": config["password"]
-}
+    data = {
+
+    "email": config["email"],
+    "password": config["password"]
+
+    }
+        
+    response = requests.post("https://sandbox.synctoskill.com/api/AccountApi/login", headers=headers, json=data)
+    assert response.status_code == 200
+    print("status code is:" + str(response.status_code))
+
+    print(response.request.body)
+    print(response.json())
+
     
-response = requests.post("https://synctoskill.com/api/AccountApi/login", headers=headers, json=data)
-assert response.status_code == 200
-print("status code is:" + str(response.status_code))
-
-print(response.request.body)
-
-print(response.json())
