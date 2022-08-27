25b927a649929638382fdb3744d97932aea94d12
Mikhail Filimonov <106674394+mikess12@users.noreply.github.com>
2022-08-27 12:06:13 +0300

    Update test_login.py

--- test_login.py
+++ test_login.py
@@ -9,15 +9,15 @@ def test_setup():
 def test_login():
     browser.get('https://synctoskill.com/')
     browser.maximize_window()
-    browser.find_elements("xpath", '//a[@class="nav-link text-dark"]')[0].click() #кликаем на кнопку логина
-    browser.find_element("name", 'Email').send_keys('sfanat@yandex.ru')   #вводим почту
+    browser.find_elements("xpath", '//a[@class="nav-link text-dark"]')[0].click() 
+    browser.find_element("name", 'Email').send_keys('sfanat@yandex.ru')   
     browser.find_element("name", 'Password').send_keys('Merinda4+')
     #browser.find_element("xpath", '//input[@class="button"]').click()
     browser.find_element("xpath", '//input[@value="Sign In"]').click()
     browser.find_element("xpath", '//a[@href="/Account/Profile"]').get_attribute('text').find("Mikhail Filimonov")
 def test_search():
-    browser.find_element("xpath", '//input[@class ="searchBar"]').send_keys('avocado')  #найти строку поиска и вставить туда слово авокадо
-    browser.find_element("xpath", '//input[@class ="searchBar"]').send_keys(Keys.ENTER) #нажать клавишу Enter
+    browser.find_element("xpath", '//input[@class ="searchBar"]').send_keys('avocado') 
+    browser.find_element("xpath", '//input[@class ="searchBar"]').send_keys(Keys.ENTER) 
     # browser.get_screenshot_as_file('3.png') 
     time.sleep(5)
     browser.close()
