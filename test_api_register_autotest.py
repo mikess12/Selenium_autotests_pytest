9a6819c0ea2e27a973f04297fcfe6b32b577bcda
Mikhail Filimonov <106674394+mikess12@users.noreply.github.com>
2022-08-27 13:12:19 +0300

    Update test_api_register_autotest.py

--- test_api_register_autotest.py
+++ test_api_register_autotest.py
@@ -1,25 +1,30 @@
 import requests
+import json
+
+with open('creds_1.json', 'r') as f:
+    config = json.load(f)
 
 
 def test_api_register():
 
     headers = {
-     "User-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
+    "User-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
     }
 
     data = {
-    
-    "password": "Qwer123+",
-    "passwordConfirmation": "Qwer123+",
-    "email": "mike9238@gmail.com",
-    "fullName": "Mike Ser",
-    "address": "Lenina 11",
-    "phone": "7(926)765-63-70"
-  
+
+    "password": config["password"],
+    "passwordConfirmation": config["passwordConfirmation"],
+    "email": config["email"],
+    "fullName": config["fullName"],
+    "address": config["address"],
+    "phone": config["phone"]
+
     }
-    
+        
     response = requests.post("https://synctoskill.com/api/AccountApi/register", headers=headers, json=data)
     assert response.status_code == 200
     print("status code is:" + str(response.status_code))
 
+    print(response.request.body)
     print(response.json())
